ea967c3d6a2b40f9dd52a25346bfb2a68146eb2f
mackwage <schuydorsey@live.com>
2017-01-07 12:21:48 -0800

    Add files via upload

---
+++ conf/tango/tango_input/bin/input.py
@@ -0,0 +1,11 @@
+import requests
+from ipwhois import IPWhois
+
+r = requests.get('http://ipv4.icanhazip.com')
+
+ip = r.text
+
+ip = ip.rstrip()
+obj = IPWhois(ip)
+results = obj.lookup_whois()
+print "sensorIP="+str(ip)+", ASN="+str(results['asn'])+", ASN_Country="+str(results['asn_country_code'])+", description="+str(results['nets'][0]['description']) + ", network_name="+str(results['nets'][0]['name'])+", network_range="+str(results['nets'][0]['range'])
